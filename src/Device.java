4316be212455afec8ea46c84434ec37f3ae0f380
Adam <adamlobocki@gmail.com>
2019-05-16 02:34:43 +0200

    Zadanie zrobione

---
+++ src/Device.java
@@ -0,0 +1,27 @@
+public class Device {
+    String code;
+    String producer;
+    String type;
+    double price;
+
+
+
+    public Device(String code, String producer, String type) {
+        this.code = code;
+        this.producer = producer;
+        this.type = type;
+    }
+
+
+    public Device(String code, String producer, String type, double price) {
+        this.code = code;
+        this.producer = producer;
+        this.type = type;
+        this.price = price;
+    }
+    void getInfo(){
+        System.out.println("Kod: " + code + " producent: " + producer + " typ: " + type + " cena: " + price);
+
+    }
+
+}
